1ffb8338ca423400176e429cb5473072eb08aff5
Paulo Alves <paulovalves@outlook.com>
2021-02-09 00:17:00 -0300

    enable cors again 2

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const app = require('./src/config/express')();
 const port = app.get('port');
 const cors = require('cors');
 
-app.use(cors());
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
+  app.use(cors());
   next();
 });
 
-app.get('/api/food', (req, res, next) => {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+
 
 
 
