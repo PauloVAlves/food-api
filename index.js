5870ac35552b61722f467920b8e5d2250db14e77
Paulo Alves <paulovalves@outlook.com>
2021-02-08 23:42:31 -0300

    enable cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 const app = require('./src/config/express')();
 const port = app.get('port');
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api/food', (req, res, next) => {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 // RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.listen(port, () => {
