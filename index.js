1592923a1c812ded3868001a696e2d9148b145aa
Paulo Alves <paulovalves@outlook.com>
2021-02-09 00:43:54 -0300

    now with CORS

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 
-const app = require('./src/config/express')();
-const port = app.get('port');
+const express = require('./src/config/express');
 const cors = require('cors');
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  app.use(cors());
-  next();
-});
+const app = express();
+const port = app.get('port');
 
-
-
-
-
-// RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 });
--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,16 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 module.exports = () => {
   const app = express();
 
+  app.use(cors());
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   
   app.set('port', process.env.PORT || 5000);
 
